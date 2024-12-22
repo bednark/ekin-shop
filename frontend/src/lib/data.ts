97ef2a04aa34ae37777cc5912242fe921c9eba06
Kuba Bednarek <kubabednarekk@gmail.com>
2024-12-22 21:13:05 +0100

    Removed pnpm-lock.yaml file from .gitignore Created Dockerfile Replaced axios with next js fetch native feature

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,24 +1,41 @@
-import axios from 'axios';
 import { Product } from '@/lib/types';
 
-const API_URL = "http://localhost:5099";
+const API_URL = process.env.API_URL || 'http://localhost:5099';
 
 export const fetchProducts = async (): Promise<Product[]> => {
   try {
-    const response = await axios.get(API_URL + '/products');
-    return response.data;
+    const response = await fetch(`${API_URL}/products`, {
+      next: { revalidate: 300 },
+    });
+
+    if (!response.ok) {
+      console.error('Error fetching products:', response.statusText);
+      throw new Error(`Failed to fetch products: ${response.status}`);
+    }
+
+    const data: Product[] = await response.json();
+    return data;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching products:', error);
     throw error;
   }
 };
 
 export const fetchProduct = async (id: number): Promise<Product> => {
   try {
-    const response = await axios.get(API_URL + '/product/' + id);
-    return response.data[0];
+    const response = await fetch(`${API_URL}/product/${id}`, {
+      next: { revalidate: 300 }
+    });
+
+    if (!response.ok) {
+      console.error(`Error fetching product with id ${id}:`, response.statusText);
+      throw new Error(`Failed to fetch product with id ${id}: ${response.status}`);
+    }
+
+    const data: Product[] = await response.json();
+    return data[0];
   } catch (error) {
-    console.error('Error posting data:', error);
+    console.error(`Error fetching product with id ${id}:`, error);
     throw error;
   }
 };
